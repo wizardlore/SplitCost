545727509bc2e7270ef48f2a514572f12f7b0850
Matthew White <mwhite@wizardlore.com>
2017-06-30 12:58:51 -0400

    reworked parsing of command line arguments along with some cleanup and wording changes

--- src/com/wizardlore/SplitCost.java
+++ src/com/wizardlore/SplitCost.java
@@ -1,5 +1,5 @@
 /*
- * Command-line utility that splits a cost amount by a set of fractional percentages
+ * Command-line utility that splits an order cost amount by a set of fractional percentages
  * 
  */
 package com.wizardlore;
@@ -8,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.math.MathContext;
+//import java.math.MathContext;
 import java.math.RoundingMode;
 import java.util.ArrayList;
 
@@ -20,10 +20,12 @@ public class SplitCost {
 
   public SplitCost(String[] args) {
     BigDecimal temp;
-    BigDecimal cost = BigDecimal.valueOf(0.0);
-    BigDecimal[] fractionsOfTotal;
-    BigDecimal[] amounts = new BigDecimal[0];
-    BigDecimal one = new BigDecimal(1.0);
+    BigDecimal orderCost = null;
+    ArrayList<BigDecimal> foos = new ArrayList<>();
+    BigDecimal FractionSum = BigDecimal.valueOf(0.0);
+    FractionSum.setScale(8, RoundingMode.HALF_UP);
+    BigDecimal[] results;
+    BigDecimal one = BigDecimal.valueOf(1.0);
     boolean simpleOutput = false;
     BufferedReader br = null;
 
@@ -35,114 +37,99 @@ public class SplitCost {
         br = new BufferedReader(new InputStreamReader(System.in));
         String response;
 
-        System.out.print("Enter a cost amount to split: ");
+        System.out.print("Enter an order cost amount to split: ");
         try {
           response = br.readLine().trim();
           if (canParseAsNumber(response)) {
-            //System.out.println("cost response: " + response);
-            cost = BigDecimal.valueOf(Double.parseDouble(response));
-
+            //System.out.println("order cost response: " + response);
+            orderCost = BigDecimal.valueOf(Double.parseDouble(response));
           }
         } catch (IOException ex) {
           System.out.println("[Error]: IO error trying to read input from console. " + ex.getMessage());
         }
-        ArrayList<BigDecimal> fots = new ArrayList();
+
         BigDecimal splitsT = new BigDecimal(0.0);
         while (splitsT.compareTo(one) < 0.0) {
-          System.out.print("Enter a fraction of total: (used: " + splitsT.doubleValue() + "; remaining: " + one.subtract(splitsT).doubleValue() + " ) ");
+          System.out.print("Enter a fraction of order: (used: " + splitsT.doubleValue() + "; remaining: " + one.subtract(splitsT).doubleValue() + " ) ");
           try {
             response = br.readLine().trim();
-
             if (canParseAsNumber(response)) {
-              System.out.println("fraction of total response: " + response);
+              //System.out.println("fraction of order response: " + response);
               if (splitsT.add(BigDecimal.valueOf(Double.parseDouble(response))).compareTo(one) > 0.0) {
-                System.out.println("Entry ignored: used would exceed 100% .");
+                System.out.println("Fraction Entry ignored. Would exceed 100%");
               } else {
                 splitsT = splitsT.add(BigDecimal.valueOf(Double.parseDouble(response)));
-                fots.add(BigDecimal.valueOf(Double.parseDouble(response)));
+                foos.add(BigDecimal.valueOf(Double.parseDouble(response)));
               }
             } else {
-              System.out.println("Unable to parse input as a number.");
+              System.out.println("Unable to parse input as a number. Please try again.");
             }
           } catch (IOException ex) {
             System.out.println("[Error]: IO error trying to read input from console. " + ex.getMessage());
           }
         }
-        fractionsOfTotal = new BigDecimal[fots.size()];
-        for (int i = 0; i < fots.size(); i++) {
-          fractionsOfTotal[i] = fots.get(i);
-        }
-
       } else {
-                // handle command line argument input
-
+        // parse command line arguments
         int index = 0;
-
-        // first argument should be the cost to split apart or an -s
-        if (args[0] != null) {
-          if (args[index].equalsIgnoreCase("-h")) {
-            printUsage();
-            System.exit(0);
+        for (String arg : args) {
+          if (arg != null) {
+            if (canParseAsNumber(arg)) {
+              if (orderCost == null) {
+                orderCost = BigDecimal.valueOf(Double.parseDouble(arg));
+              } else {
+                foos.add(BigDecimal.valueOf(Double.parseDouble(arg)));
+                FractionSum = FractionSum.add(BigDecimal.valueOf(Double.parseDouble(arg)));
+              }
+            } else {
+              //check if arg is a command trigger
+              if (arg.equalsIgnoreCase("-h")
+                      || args[index].equalsIgnoreCase("-sh")
+                      || args[index].equalsIgnoreCase("-hs")) {
+                // display usage help
+                printUsage();
+                return;
+              } else if (arg.equalsIgnoreCase("-s")) {
+                // simple output
+                simpleOutput = true;
+              } else {
+                // unknown 
+                System.out.println("-1: Unable to parse argument \""+arg+"\" as a number or command trigger.");
+              }
+            }
           }
-          // check if first arg is -s
-          if (args[index].equalsIgnoreCase("-s")) {
-            index = 1;
-            simpleOutput = true;
-          }
-
-          if ((args.length < 2 && index == 0) || (args.length < 3 && index == 1)) {
-            System.out.println("Not enough arguments.");
-            printUsage();
-            System.exit(-2);
-          }
-
-          if (canParseAsNumber(args[index])) {
-            cost = BigDecimal.valueOf(Double.parseDouble(args[index]));
-          } else {
-            System.out.println("An argument is not parsable as a number.");
-            printUsage();
-            System.exit(-3);
-          }
-        }
-
-        fractionsOfTotal = new BigDecimal[args.length - index - 1];
-        amounts = new BigDecimal[args.length - index - 1];
-
-        // followed by a list of fractions of total
-        BigDecimal FractionSum = BigDecimal.valueOf(0.0);
-        FractionSum.setScale(8, RoundingMode.HALF_UP);
-        for (int i = 0; i < fractionsOfTotal.length; i++) {
-          if (canParseAsNumber(args[i + 1])) {
-            fractionsOfTotal[i] = BigDecimal.valueOf(Double.parseDouble(args[i + 1 + index]));
-            FractionSum = FractionSum.add(fractionsOfTotal[i]);
-          } else {
-            System.out.println("An argument is not parsable as a number.");
-            printUsage();
-            System.exit(-3);
-          }
-        }
-
-        // the sum of the fractionsOfTotal array must equal 1 aka 100%
-        if (FractionSum.compareTo(one) != 0.0) {
-          System.out.println("The sum of the \"fractions of total\" does not equal 1.0");
-          System.exit(-4);
         }
 
       }
 
+      //verify input
+      if (orderCost != null) {
+        //the sum of the Fractions Of Order must equal 1 aka 100%
+        if (FractionSum.compareTo(one) == 0.0) {
+          results = computeSplitAmounts(orderCost, foos);
+        } else {
+          System.out.println("-3: The sum of \"Fractions Of Order\" does not equal 1.0");
+          return;
+        }
+      } else {
+        System.out.println("-2: Unable to parse required Order Cost argument.");
+        printUsage();
+        return;
+      }
+
+      //output
       if (!simpleOutput) {
-        System.out.println("Cost to split: " + cost);
-        System.out.print("Fractions of total: ");
-        for (int i = 0; i < fractionsOfTotal.length; i++) {
-          if (i < fractionsOfTotal.length - 1) {
-            System.out.print(fractionsOfTotal[i] + ", ");
+        System.out.println("Order Cost to split: " + orderCost);
+        System.out.print("Fractions of Order: ");
+        int i = 1;
+        for (BigDecimal foo : foos) {
+          if (i < foos.size()) {
+            System.out.print(foo + ", ");
           } else {
-            System.out.println(fractionsOfTotal[i]);
+            System.out.println(foo);
           }
+          i++;
         }
       }
-
-      BigDecimal[] results = computeSplitAmounts(cost, fractionsOfTotal);
 
       if (results != null) {
         if (simpleOutput) {
@@ -164,9 +151,9 @@ public class SplitCost {
           System.out.println("Splits Total: " + temp);
         }
       } else {
-        System.out.println("no results");
+        System.out.println("no results produced");
       }
-
+      // continue user interaction, if any
       if (br != null) {
         System.out.println("");
         System.out.print("Would you like to calculate another split cost? [y/n] ");
@@ -178,59 +165,66 @@ public class SplitCost {
         } catch (IOException ex) {
           System.out.println("[Error]: IO error trying to read input from console. " + ex.getMessage());
         }
-
       }
-
     } else {
+      // this should never be reached
       // show usage message
       System.out.println("Args was null");
       printUsage();
-      System.exit(-1);
     }
   }
 
-  private BigDecimal[] computeSplitAmounts(BigDecimal cost, BigDecimal[] fractionsOfTotal) {
+  private BigDecimal[] computeSplitAmounts(BigDecimal cost, ArrayList<BigDecimal> fractionsOfOrder) {
     BigDecimal one = new BigDecimal(1.0);
-    BigDecimal[] amounts = new BigDecimal[fractionsOfTotal.length];
+    BigDecimal[] amounts = new BigDecimal[fractionsOfOrder.size()];
     BigDecimal FractionSum = BigDecimal.valueOf(0.0);
     FractionSum.setScale(8, RoundingMode.HALF_UP);
 
-    for (BigDecimal aFractionsOfTotal : fractionsOfTotal) {
-      FractionSum = FractionSum.add(aFractionsOfTotal);
-      //System.out.println("FractionSum: " + FractionSum.doubleValue());
+    for (BigDecimal aFractionsOfOrder : fractionsOfOrder) {
+      FractionSum = FractionSum.add(aFractionsOfOrder);
     }
 
-    // the sum of the fractionsOfTotal array must equal 1 aka 100%
+    // the sum of the fractionsOfOrder array must equal 1 aka 100%
     if (FractionSum.compareTo(one) != 0.0) {
-      //System.out.println("Sum of Fractions do not equal 1.0: " + FractionSum);
       return null;
     }
 
     // calculate split amounts
     BigDecimal temp = BigDecimal.valueOf(0.0);
-    for (int i = 0; i < fractionsOfTotal.length; i++) {
-      amounts[i] = cost.multiply(fractionsOfTotal[i]).setScale(2, RoundingMode.HALF_UP);
+    for (int i = 0; i < fractionsOfOrder.size(); i++) {
+      amounts[i] = cost.multiply(fractionsOfOrder.get(i)).setScale(2, RoundingMode.HALF_UP);
       temp = temp.add(amounts[i]);
     }
     BigDecimal diff = cost.subtract(temp);
     if (diff.compareTo(BigDecimal.valueOf(0.0)) != 0.0) {
-      amounts[amounts.length - 1] = amounts[amounts.length - 1].add(diff);
-      //System.out.println("Leftover diffence amount was applied to last split: " + diff.doubleValue());
+      // apply difference to largest split
+      int largestSplitIndex = getIndexofLargest(amounts);
+      amounts[largestSplitIndex] = amounts[largestSplitIndex].add(diff);
+      System.out.print("Applied Diff of " + diff + ", to index: " + largestSplitIndex);
+
     }
     return amounts;
   }
 
   private void printUsage() {
-    System.out.println("\r\nBasic usage:");
-    System.out.println("\r\n  java -jar SplitCost.jar [-sh] [CostToSplit] [FractionsOfTotal, ...] ");
-    System.out.println("\r\nexamples:");
-    System.out.println("\r\n  java -jar SplitCost.jar 101.01 .3333 .3333 .3334");
-    System.out.println("\r\n  -s Output split amounts only:");
-    System.out.println("\r\n    java -jar SplitCost.jar -s 101.01 .5 .25 .10 .10 .05");
-    System.out.println("\r\n  -h Output this help message:");
-    System.out.println("\r\n    java -jar SplitCost.jar -h");
-    System.out.println("\r\nExecute with no arguments to run interactively:");
-    System.out.println("\r\n  java -jar SplitCost.jar\r\n");
+    System.out.println("Basic usage:");
+    System.out.println("  java -jar SplitCost.jar [-sh] [OrderCostToSplit] [FractionsOfOrder ...] ");
+    System.out.println("");
+    System.out.println("OrderCostToSplit - numeric order total amount");
+    System.out.println("FractionsOfOrders - space separated list of the fractions to split the order by.");
+    System.out.println("  The sum of the list of Fractions of Order must equal 1.0");
+    System.out.println("");
+    System.out.println("examples:");
+    System.out.println("  java -jar SplitCost.jar 101.01 .3333 .3333 .3334");
+    System.out.println("");
+    System.out.println("-s Output split amounts only:");
+    System.out.println("  java -jar SplitCost.jar -s 101.01 .5 .25 .10 .10 .05");
+    System.out.println("");
+    System.out.println("-h Output this help message:");
+    System.out.println("  java -jar SplitCost.jar -h");
+    System.out.println("");
+    System.out.println("Execute with no arguments to run interactively:");
+    System.out.println("  java -jar SplitCost.jar\r\n");
   }
 
   private boolean canParseAsNumber(String arg) {
@@ -242,10 +236,25 @@ public class SplitCost {
     return true;
   }
 
+  private int getIndexofLargest(BigDecimal[] amounts) {
+    int i = 0;
+    int index = 0;
+    double largest = Double.MIN_VALUE;
+    for (BigDecimal amount : amounts) {
+      if (amount.doubleValue() > largest) {
+        largest = amount.doubleValue();
+        index = i;
+      }
+      i++;
+    }
+    return index;
+  }
+
   /**
    * @param args the command line arguments
    */
   public static void main(String[] args) {
     SplitCost splitCost = new SplitCost(args);
   }
+
 }
